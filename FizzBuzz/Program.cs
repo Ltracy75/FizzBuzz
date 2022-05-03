805ea291f70c92e7794dfff68591fe71ab68ce26
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-03 16:24:01 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+for (var idx = 1; idx <= 30; idx++)
+    {
+    if ((idx % 3 == 0) && (idx % 5 == 0))
+        Console.WriteLine($"Fizzbuzz, {idx}");
+
+    else if (idx % 3 == 0)
+        Console.WriteLine($"Fizz, {idx}");
+
+    else if (idx % 5 == 0)
+        Console.WriteLine($"Buzz, {idx}");
+    else
+        Console.WriteLine($"{idx}");
+    }
+
+
